b6492295d6b3b332926abccc8ca6d6ac43615081
Fangwen Zhao <fzhao@cemm.at>
2025-08-06 16:28:56 +0200

    Added addition of retrieving pycistarget motif hits

--- workflow/scripts/aggregate.py
+++ workflow/scripts/aggregate.py
@@ -47,7 +47,7 @@ if len(results_list)==0:
 # concatenate all results into one results dataframe
 result_df = pd.concat(results_list, axis=0)
 
-# save all enirchment results
+# save all enrichment results
 result_df.to_csv(results_all_path)
 
 # find union of statistically significant terms
@@ -61,3 +61,34 @@ result_sig_df = result_df.loc[result_df[term_col].isin(sig_terms), :]
 
 # save filtered enirchment results by significance
 result_sig_df.to_csv(results_sig_path)
+
+# If pycistarget, then also save motif hits
+if tool == "pycisTarget":
+    hits_list = []
+    cistrome_list = []
+    
+    for result_path in result_paths:
+        if os.stat(result_path).st_size != 0:
+            tmp_name = os.path.basename(result_path).replace("_{}.csv".format(db),"")
+            
+            hits_path = result_path.replace(".csv", ".motif_hits.csv")
+            cistrome_path = result_path.replace(".csv", ".cistromes.csv")
+            
+            # add hits to results
+            tmp_res = pd.read_csv(hits_path)
+            tmp_res['name'] = tmp_name
+            hits_list.append(tmp_res)
+            
+            # add cistrome to results
+            tmp_res = pd.read_csv(cistrome_path)
+            tmp_res['name'] = tmp_name
+            cistrome_list.append(tmp_res)
+
+    # concatenate all results into one results dataframe
+    hits_df = pd.concat(hits_list, axis=0)
+    hits_df.to_csv(results_all_path.replace(".csv", ".motif_hits.csv"))
+
+    cistrome_df = pd.concat(cistrome_list, axis=0)
+    cistrome_df.to_csv(results_all_path.replace(".csv", ".cistromes.csv"))
+
+
