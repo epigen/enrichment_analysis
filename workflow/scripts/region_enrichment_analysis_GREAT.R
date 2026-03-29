7a4888261f54942940ce069e14d49e5aace9fa96
Martin <martstoll29@gmail.com>
2026-03-29 20:50:20 +0200

    new PR

--- workflow/scripts/region_enrichment_analysis_GREAT.R
+++ workflow/scripts/region_enrichment_analysis_GREAT.R
@@ -5,6 +5,44 @@ library("rGREAT")
 library("data.table")
 library("rtracklayer")
 
+# functions
+annot_terms_with_features <- function(res, df) {
+  regions <- vector("character", nrow(df))
+  genes <- vector("character", nrow(df))
+
+  needs_annotation <- rep(TRUE, nrow(df))
+  if ("p_adjust" %in% names(df)) {
+    needs_annotation <- needs_annotation & (is.na(df$p_adjust) | df$p_adjust <= snakemake@config[["adjp_th"]][["GREAT"]])
+  }
+  
+
+  term_ids <- unique(df$id[needs_annotation])
+  annotations <- setNames(vector("list", length(term_ids)), term_ids)
+
+  for (term in term_ids) {
+    gr <- getRegionGeneAssociations(res, term_id = term)
+    annotations[[term]] <- list(
+      regions = paste(
+        paste0(seqnames(gr), ":", pmax(0L, start(gr) - 1L), "-", end(gr)), # output the file in a bed convention format (open start, closed end)
+        collapse = ","
+      ),
+      annotated_genes = paste(unique(unlist(gr$annotated_genes)), collapse = ",")
+    )
+  }
+
+  for (i in which(needs_annotation)) {
+    annotation <- annotations[[df$id[i]]]
+    if (!is.null(annotation)) {
+      regions[i] <- annotation$regions
+      genes[i] <- annotation$annotated_genes
+    }
+  }
+
+  df$regions <- regions
+  df$annotated_genes <- genes
+
+  return(df)
+}
 # configs
 
 #input
@@ -62,4 +100,6 @@ res <- great(gr = regionSet_query,
 # get & save result table
 tb <- getEnrichmentTable(res, min_region_hits = 0)
 tb$description <- paste(tb$description, tb$id)
+# annotate (near) significant enrichments with features
+tb <- annot_terms_with_features(res, tb)
 fwrite(as.data.frame(tb), file=file.path(result_path), row.names=FALSE)
